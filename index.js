47321f49e1237ef23eb3b97a9cae80d0f1dcbcf0
Tom <yudaihammer@gmail.com>
2020-10-08 06:25:46 -0700

    port from environment variable

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {  
   console.log(`Starting server at ${port}`);
 });
